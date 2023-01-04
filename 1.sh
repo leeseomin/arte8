f7231d2b17aba5e0a1638bb6e56d78333fb0a442
LEE SEOMIN <86279699+leeseomin@users.noreply.github.com>
2023-01-04 09:51:23 +0900

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,29 @@
+#only sket
+
+parallel --no-notice rm -r ::: s1/* s2/* s3/* s4/* s5/* s6/* s7/* s8/* s9/* s10/* s11/* s12/* s13/*
+parallel --no-notice rm -r ::: s14/* s15/* s16/* s17/* s18/* s19/* s20/* s21/* s22/* s23/* s24/* s25/*
+cd s
+for f in *\ *; do mv "$f" "${f// /_}"; done
+cd ..
+cd s
+parallel convert {} -resize 3500x3500 {.}.png ::: *.* 
+rm *.jpg
+rm *.JPG
+parallel convert {} ../s25/{} ::: *.*
+cd ..
+cd s
+parallel convert {} -modulate 100,150,100  ../s1/{} ::: *.* 
+cd ..
+cd s1
+for i in *.* 
+do 
+convert $i ../s/$i -alpha set -channel A -evaluate set 100% -compose softlight -composite ../s3/$i 
+done 
+cd .. 
+cd s3
+parallel gmic {} -fx_poster_edges 20,30,5,0,10,0,0,0 -o ../s4/{} ::: *.* 
+cd ..
+cd s4
+for i in *.*; do
+   convert $i  -set filename:new ../s25/"%tsket" "%[filename:new].png"
+done
